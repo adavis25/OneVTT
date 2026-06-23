b348d57cb328cba68d7fd46eead5d0586735129b
Hans Koenig <HKoenig3@github.com>
2026-06-23 14:54:39 -0500

    TypeScript updates for character

---
+++ docs/TYPESCRIPT_NOTES.md
@@ -0,0 +1,5 @@
+# Refer to DND Beyond character sheet (https://www.dndbeyond.com/characters/37018837)
+
+dnd beyond -> background tab -> lore interface typescript
+notes -> lore
+things derive from AbilityScore
--- pluigns/dnd5e/src/schemas/character.ts
+++ pluigns/dnd5e/src/schemas/character.ts
@@ -3,17 +3,51 @@ interface AbilityScore {
   modifier: number;
 }
 
-interface Character {
+interface SavingThrow {
+  modifier: number;
+  proficiency: boolean;
+}
+
+interface Skill {
+  modifier: number;
+  proficiency: boolean;
+}
+
+interface Lore {
   name: string;
-  level: number;
   race: string;
   class: string;
   background: string;
-  alignment: string;
+  alignment: Enumerator;
+  gender: string;
+  personality: string[];
+  ideals: string[];
+  bonds: string[];
+  flaws: string[];
+  traits: string[];
+}
+
+interface Character {
+  name: string;
+  level: number;
+  playername: string;
   experience: number;
   hitPoints: number;
+  tempHitPoints: number;
   armorClass: number;
+  initiative: number;
   speed: number;
+  lore: Lore;
+  skills: Skill[];
+
+  savingThrows: {
+    strength: SavingThrow;
+    dexterity: SavingThrow;
+    constitution: SavingThrow;
+    intelligence: SavingThrow;
+    wisdom: SavingThrow;
+    charisma: SavingThrow;
+  };
 
   abilities: {
     strength: AbilityScore;
